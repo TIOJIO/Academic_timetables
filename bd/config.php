cd35a056978e16ed2882b3b4e35a2042f3225bf6
tiojio <tiojioromain02@gmail.com>
2023-05-03 02:59:50 +0100

    update connection to azure

--- bd/config.php
+++ bd/config.php
@@ -1,5 +1,7 @@
 <?php
-   
+   $options = array();
+   $options[PDO::MYSQL_ATTR_SSL_CA] = '/path/to/ca-cert.pem' ;
+   $options[PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT] = false ;
     try {
          $pdo = new PDO("mysql:host=stima-db.mysql.database.azure.com;Database=stima_db", "TIOJIO", "tiojio15#");
     } catch (\Throwable $th) {
